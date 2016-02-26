a692ca2e34be3a8887b45e846ea72307ab3832e7
Taliesin Sisson <taliesins@yahoo.com>
2016-02-26 17:18:20 +0000

    Add IIS configuration for SageCrm

---
+++ recipes/iis.rb
@@ -0,0 +1,30 @@
+#
+# Cookbook Name:: sagecrm
+# Recipe:: default
+#
+# Copyright (C) 2015 Taliesin Sisson
+#
+# All rights reserved - Do Not Redistribute
+#
+
+iis_pool 'CRM App Pool' do
+  runtime_version "2.0"
+  pipeline_mode :Integrated
+  thirty_two_bit : true
+  action :add
+end
+
+iis_site 'Default Web Site' do
+  protocol :http
+  port 80
+  path "C:\\inetpub\\WWWRoot"
+  action [:add,:start]
+end
+
+iis_app 'CRM' do
+  path "/CRM"
+  application_pool 'CRM App Pool'
+  physical_path "#{node['sagecrm']['instance']['install_dir']}CRM\\WWWRoot"
+  enabled_protocols "http"
+  action :add
+end
