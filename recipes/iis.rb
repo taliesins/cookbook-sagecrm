bcfdd8ee254cbb642a87e52502891de96afed0e9
Taliesin Sisson <taliesins@yahoo.com>
2016-02-29 12:16:31 +0000

    Notify windows_service resource

--- recipes/iis.rb
+++ recipes/iis.rb
@@ -7,30 +7,6 @@
 # All rights reserved - Do Not Redistribute
 #
 
-iis_config "/section:system.webServer/asp /enableParentPaths:\"True\" /commit:apphost" do
-  action :set
-  notifies :restart, 'service[W3SVC]'
-end
-
-iis_config "/section:system.webServer/asp /scriptErrorSentToBrowser:\"True\" /commit:apphost" do
-  action :set
-  notifies :restart, 'service[W3SVC]'
-end
-
-iis_config "/section:anonymousAuthentication /username:\"\" --password" do
-  action :set
-  notifies :restart, 'service[W3SVC]'
-end
-
-iis_config "/section:handlers /accessPolicy:Read,Script,Execute" do
-  action :set
-  notifies :restart, 'service[W3SVC]'
-end
-
-windows_service 'W3SVC' do
-  action :nothing
-end
-
 iis_pool node['sagecrm']['website']['main']['application_pool'] do
   runtime_version '2.0'
   pipeline_mode :Integrated
@@ -61,4 +37,28 @@ iis_app 'CRM' do
   physical_path node['sagecrm']['application']['crm']['physical_path']
   enabled_protocols node['sagecrm']['application']['crm']['enabled_protocols']
   action :add
+end
+
+iis_config "/section:system.webServer/asp /enableParentPaths:\"True\" /commit:apphost" do
+  action :set
+  notifies :restart, 'windows_service[W3SVC]'
+end
+
+iis_config "/section:system.webServer/asp /scriptErrorSentToBrowser:\"True\" /commit:apphost" do
+  action :set
+  notifies :restart, 'windows_service[W3SVC]'
+end
+
+iis_config "/section:anonymousAuthentication /username:\"\" --password" do
+  action :set
+  notifies :restart, 'windows_service[W3SVC]'
+end
+
+iis_config "/section:handlers /accessPolicy:Read,Script,Execute" do
+  action :set
+  notifies :restart, 'windows_service[W3SVC]'
+end
+
+windows_service 'W3SVC' do
+  action :nothing
 end
